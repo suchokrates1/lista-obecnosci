185a528a12b04e50f9d85ec49740304e5316d3a1
suchokrates1 <dawsuchodolski@gmail.com>
2025-06-07 19:35:45 +0200

    Remove unused imports

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
