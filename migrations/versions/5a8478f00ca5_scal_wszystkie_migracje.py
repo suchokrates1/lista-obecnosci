467538e859584050f8fd9c542005ebc711388f1a
suchokrates1 <dawsuchodolski@gmail.com>
2025-12-21 18:06:59 +0100

    Fix migration chain - add merge migration and update invoice migration parent

---
+++ migrations/versions/5a8478f00ca5_scal_wszystkie_migracje.py
@@ -0,0 +1,24 @@
+"""scal wszystkie migracje
+
+Revision ID: 5a8478f00ca5
+Revises: 46accc2150cf
+Create Date: 2025-06-16 23:30:32.560710
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5a8478f00ca5'
+down_revision = '46accc2150cf'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
